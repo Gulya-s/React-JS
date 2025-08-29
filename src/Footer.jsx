ee7468bb2b9f2a3e1f6a997238e972c819e345ae
Guliya <adilgulsezim11@gmail.com>
2025-08-29 21:27:59 +0500

    first commit

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer style={{background: "222c22", padding: "15px", color:"white", marginTop:"24px"}}>
-            <p>2005 My React App</p>
-        </footer>
-    )
-}
-
-export default Footer;
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return(
-        <header style={{background: "222c22", padding: "15px", color:"white"}}>
-            <h2>My React App</h2>
-        </header>
-    )
-}
-
-export default Header;
